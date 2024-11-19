21de6df945a1450a38296cd3efa2960b2dd4d00a
LeopoldHsing <LeopoldHsing@gmail.com>
2024-11-18 20:18:19 -0600

    build: add dockerfile

---
+++ src/api/OrderRequest.ts
@@ -0,0 +1,42 @@
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
+const FRONTEND_ENDPOINT = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+
+const getOrderRequest = async ({ orderId, accessToken }: { orderId: string, accessToken: string }) => {
+  const response = await fetch(`${BASE_URL}/api/stripe/order/${orderId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    }
+  })
+  if (!response.ok) {
+    throw new Error(`Failed to retrieve order ${orderId}`)
+  }
+  return response.json();
+}
+
+const createPayloadOrder = async ({ userId, productPayloadIds, isPaid, payloadToken }: {
+  userId: string,
+  productPayloadIds: string[],
+  isPaid: boolean,
+  payloadToken: string
+}) => {
+  const response = await fetch(`${FRONTEND_ENDPOINT}/api/orders`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${payloadToken}`
+    },
+    body: JSON.stringify({
+      user: userId,
+      products: productPayloadIds,
+      _isPaid: isPaid
+    })
+  })
+  if (!response.ok) {
+    throw new Error("Failed to create order in Payload")
+  }
+  return response.json()
+}
+
+export { getOrderRequest, createPayloadOrder }
