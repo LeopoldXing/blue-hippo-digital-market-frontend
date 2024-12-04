4b3499493476701085a09ff01392a4f191d95c36
LeopoldHsing <LeopoldHsing@gmail.com>
2024-12-03 21:53:06 -0600

    feat: add breadcrumbs

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -18,6 +18,7 @@ interface PageProps {
 const BREADCRUMBS = [
   { id: 1, name: 'Home', href: '/' },
   { id: 2, name: 'Products', href: '/products' },
+  { id: 3, name: "111", href: `/product/${1}` }
 ]
 
 const Page = async ({ params }: PageProps) => {
@@ -38,19 +39,35 @@ const Page = async ({ params }: PageProps) => {
             <div className='lg:max-w-lg lg:self-end'>
               <ol className='flex items-center space-x-2'>
                 {BREADCRUMBS.map((breadcrumb, i) => (
-                    <li key={breadcrumb.href}>
-                      <div className='flex items-center text-sm'>
-                        <Link href={breadcrumb.href} className='font-medium text-sm text-muted-foreground hover:text-gray-900'>
-                          {breadcrumb.name}
-                        </Link>
-                        {i !== BREADCRUMBS.length - 1 ? (
-                            <svg viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'
-                                 className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
-                              <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z'/>
-                            </svg>
-                        ) : null}
-                      </div>
-                    </li>
+                    i === 2 ? (
+                        <li key={breadcrumb.href}>
+                          <div className='flex items-center text-sm'>
+                            <Link href={`http://localhost:3000/product/${product.id}`} className='font-medium text-sm text-muted-foreground hover:text-gray-900'>
+                              {product.name}
+                            </Link>
+                            {i !== BREADCRUMBS.length - 1 ? (
+                                <svg viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'
+                                     className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
+                                  <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z'/>
+                                </svg>
+                            ) : null}
+                          </div>
+                        </li>
+                        ) : (
+                        <li key={breadcrumb.href}>
+                          <div className='flex items-center text-sm'>
+                            <Link href={breadcrumb.href} className='font-medium text-sm text-muted-foreground hover:text-gray-900'>
+                              {breadcrumb.name}
+                            </Link>
+                            {i !== BREADCRUMBS.length - 1 ? (
+                                <svg viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'
+                                     className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
+                                  <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z'/>
+                                </svg>
+                            ) : null}
+                          </div>
+                        </li>
+                    )
                 ))}
               </ol>
 
